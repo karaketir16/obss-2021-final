f1b6e8d00a4d4d63a25534cae9bfefbcd87a8be7
Osman Karaketir <osmankaraketir@gmail.com>
2021-05-08 12:53:43 +0300

    fixed task 3

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,45 @@ const express = require('express');
 const router = express.Router();
 const axios = require("axios")
 /* GET home page. */
-router.get('/api/v1/echo/:queryText', function(req, res, next) {
-  res.send(req.params['queryText']);
+router.get('/api/v1/echo/:queryText', function (req, res, next) {
+    res.send(req.params['queryText']);
 });
 
-router.get('/api/v1/issuetypes', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  axios.get(req.query.jiraUrl + "rest/api/2/issuetype").then(jira => {
-    // let not_subtask = []
-    // for(let i = 0; i < jira.data.length; i++){
-    //   if( ! jira.data[i]["subtask"]){
-    //     console.log(jira.data);
-    //     not_subtask.push(jira.data[i]);
-    //   }
-    // }
-    // res.json(not_subtask);
-    res.json(jira.data);
-  });
+router.get('/api/v1/issuetypes', function (req, res, next) {
+    // res.render('index', { title: 'Express' });
+    axios.get(req.query.jiraUrl + "rest/api/2/issuetype").then(jira => {
+        // let not_subtask = []
+        // for(let i = 0; i < jira.data.length; i++){
+        //   if( ! jira.data[i]["subtask"]){
+        //     console.log(jira.data);
+        //     not_subtask.push(jira.data[i]);
+        //   }
+        // }
+        // res.json(not_subtask);
+        res.json(jira.data);
+    });
 });
 
-router.get('/api/v1/issues/subtasks', async function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  console.log(req.query.jiraUrl)
-  let jira = await axios.get(req.query.jiraUrl + "rest/api/2/project/" + req.query.projectId);
-  // let jira = await axios.get(req.query.jiraUrl + "rest/api/2/project/" + req.query.projectId + "/components");
-  // let issues = await axios.get(req.query.jiraUrl + "rest/api/2/component/" + jira.data.id +"/relatedIssueCounts");
-  // /rest/api/2/component/{id}/relatedIssueCounts
-  // console.log(jira.data);
-  let subtask = []
-  for(let i = 0; i < jira.data.issueTypes.length; i++){
-    if( jira.data.issueTypes[i]["subtask"]){
-      // console.log(jira.data.issueTypes);
-      console.log(jira.data);
-      let issue = await axios.get(req.query.jiraUrl + "rest/api/2/issue/" + jira.data.issueTypes[i].id);
-      subtask.push(issue.data);
+router.get('/api/v1/issues/subtasks', async function (req, res, next) {
+
+    let jira = await axios.get(req.query.jiraUrl + "rest/api/2/search", {params: {
+        jql:"project=" + req.query.projectId, maxResults:999999
+    }})
+
+    // console.log(jira.data)
+    // return null;
+
+    let subtask = []
+
+    for (let i = 0; i < jira.data.issues.length; i++) {
+        if (jira.data.issues[i].fields.issuetype.subtask) {
+            subtask.push(jira.data.issues[i])
+        }
     }
-  }
     // res.json(not_subtask);
-  res.json(subtask);
+    res.json({issues: subtask});
 
 });
-
 
 
 module.exports = router;

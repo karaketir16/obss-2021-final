f266c5811b32bb8224541b02f1e68c2e58e0d609
Osman Karaketir <osmankaraketir@gmail.com>
2021-05-08 13:24:48 +0300

    done task 4

--- routes/index.js
+++ routes/index.js
@@ -42,5 +42,60 @@ router.get('/api/v1/issues/subtasks', async function (req, res, next) {
 
 });
 
+router.post('/api/v1/users/find-top-n-users', async function (req, res, next) {
+
+    let projects = req.body
+    console.log(req.body);
+
+    let all_issues = [];
+    for (const project of projects){
+        let jira = await axios.get(req.query.jiraUrl + "rest/api/2/search", {params: {
+                jql:"project=" + project, maxResults:999999
+        }})
+        all_issues.push(...jira.data.issues);
+    }
+    let users = {
+
+    };
+
+    // console.log(all_issues);
+
+    for (const issue of all_issues){
+        let user = issue.fields.assignee;
+        if(user){
+            if(users[user.key] === undefined){
+                users[user.key] = user;
+                users[user.key].issueCount = 1;
+            } else {
+                users[user.key].issueCount += 1;
+            }
+        }
+    }
+
+    // console.log(users)
+    let user_list = []
+    for (const key in users){
+        user_list.push(users[key]);
+    }
+    user_list.sort((a,b)=> {
+        if (a.issueCount === b.issueCount){
+            return a.name > b.name ? 1 : -1;
+        }
+        return a.issueCount < b.issueCount ? 1 : -1;
+    })
+    console.log(user_list)
+    let ans = user_list.slice(0, req.query.topn);
+    // let subtask = []
+
+    // for (let i = 0; i < jira.data.issues.length; i++) {
+    //     if (jira.data.issues[i].fields.issuetype.subtask) {
+    //         subtask.push(jira.data.issues[i])
+    //     }
+    // }
+    // res.json(not_subtask);
+    res.json(ans);
+
+});
+
 
 module.exports = router;

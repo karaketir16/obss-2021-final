422e6df8c67fedf472b25164d953ed70ee5f5191
Osman Karaketir <osmankaraketir@gmail.com>
2021-05-08 13:34:24 +0300

    done task 5

--- routes/index.js
+++ routes/index.js
@@ -83,18 +83,60 @@ router.post('/api/v1/users/find-top-n-users', async function (req, res, next) {
         }
         return a.issueCount < b.issueCount ? 1 : -1;
     })
-    console.log(user_list)
+    // console.log(user_list)
     let ans = user_list.slice(0, req.query.topn);
-    // let subtask = []
 
-    // for (let i = 0; i < jira.data.issues.length; i++) {
-    //     if (jira.data.issues[i].fields.issuetype.subtask) {
-    //         subtask.push(jira.data.issues[i])
-    //     }
-    // }
-    // res.json(not_subtask);
     res.json(ans);
+});
+// http://localhost:8080/api/v1/projects/find-min-n-issues?jiraUrl={jiraUrl}&minn={minn}&topm={topm}
+router.post('/api/v1/projects/find-min-n-issues', async function (req, res, next) {
 
+    let minn = req.query.minn ? req.query.minn : 5;
+    let topm = req.query.topm ? req.query.topm : 10;
+
+    let users = req.body;
+
+    let all_issues = [];
+    for (const user of users){
+        let jira = await axios.get(req.query.jiraUrl + "rest/api/2/search", {params: {
+                jql:"assignee=" + user, maxResults:999999
+            }})
+        all_issues.push(...jira.data.issues);
+    }
+
+    let projects = {};
+
+    // console.log(all_issues);
+
+    for (const issue of all_issues){
+        let project = issue.fields.project;
+        if(project){
+            if(projects[project.key] === undefined){
+                projects[project.key] = project;
+                projects[project.key].issueCount = 1;
+            } else {
+                projects[project.key].issueCount += 1;
+            }
+        }
+    }
+
+    // console.log(users)
+    let project_list = []
+    for (const key in projects){
+        if(projects[key].issueCount >= minn){
+            project_list.push(projects[key]);
+        }
+    }
+    project_list.sort((a,b)=> {
+        if (a.issueCount === b.issueCount){
+            return a.name > b.name ? 1 : -1;
+        }
+        return a.issueCount < b.issueCount ? 1 : -1;
+    })
+    // console.log(user_list)
+    let ans = project_list.slice(0, topm);
+
+    res.json(ans);
 });
 
 
